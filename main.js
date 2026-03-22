3a9c01d06a412a5883c369b3257c882a412ff163
HolmfridurBiology <holmfridur@flensborg.is>
2026-03-22 12:39:52 +0000

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="120" height="60" viewBox="0 0 120 60">
-  <rect width="120" height="60" rx="8" fill="#006633"/>
-  <text x="60" y="26" font-family="Verdana" font-size="17" font-weight="bold" fill="white" text-anchor="middle">VMA</text>
-  <text x="60" y="42" font-family="Verdana" font-size="7" fill="#aaddbb" text-anchor="middle">Verkmenntaskólinn</text>
-  <text x="60" y="53" font-family="Verdana" font-size="7" fill="#aaddbb" text-anchor="middle">á Mýrum</text>
-</svg>
